346bf9448db4dc4652d29606e9af44fdb44cbd26
Dean Meier <f55004583@leeschools.net>
2016-11-30 08:00:07 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,135 @@
 /* Main.js */
-    $(document).ready(function() {
-        var delay = 0.1;
-        var scWidth = 50;
-        var scHeight = 50;
-
-        for (i=1; i<8; i++) {
-            var cssWidth = scWidth + "px";
-            var cssHeight = scHeight + "px";
-            var animation = "spin 1.5s linear " + delay + "s infinite";
-
-            $("#logo-container").append("<div class=\"square-container\" id=\"sc" + i + "\"></div>"); 
-
-            $("#sc" + i + " ").css({
-                "width": cssWidth,
-                "height": cssHeight,
-                "-webkit-animation": animation,
-                "-moz-animation": animation,
-                "-o-animation": animation,
-                "animation": animation
-            });
-
-            scWidth += 10;
-            scHeight += 10;
-            delay += 0.1;
-        }
-
-
-        var sWidth = 30;
-        var sHeight = 30;
-
-        for (i=1; i<8; i++) {
-            var cssWidth = sWidth + "px";
-            var cssHeight = sHeight + "px";
-
-
-            $("#sc" + i + " ").append("<div class=\"square\" id=\"s" + i + "\"></div>");
-
-            $("#s" + i + " ").css({
-                "width": cssWidth,
-                "height": cssHeight
-            });
-
-            sWidth += 10;
-            sHeight += 10;
-
-        }
-
+$(document).ready(function() {
+    $("#btn-1").click(function() {
+        $("#btn-1 .inner").css({
+           "background-color": "#DC3522" 
+        });
+        $("#btn-2 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-3 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-4 .inner").css({
+           "background-color": "white" 
+        });
+        
+        
+        $("#img-1").css({
+           "opacity": "1",
+           "z-index": "20"
+        });
+        $("#img-2").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-3").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-4").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
     });
+    
+    $("#btn-2").click(function() {
+        $("#btn-2 .inner").css({
+           "background-color": "#DC3522" 
+        });
+        $("#btn-1 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-3 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-4 .inner").css({
+           "background-color": "white" 
+        });
+        
+        
+        $("#img-2").css({
+           "opacity": "1",
+           "z-index": "20"
+        });
+        $("#img-1").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-3").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-4").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+    });
+    
+    $("#btn-3").click(function() {
+        $("#btn-3 .inner").css({
+           "background-color": "#DC3522" 
+        });
+        $("#btn-1 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-2 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-4 .inner").css({
+           "background-color": "white" 
+        });
+        
+        
+        $("#img-3").css({
+           "opacity": "1",
+           "z-index": "20"
+        });
+        $("#img-1").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-2").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-4").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+    });
+        
+    $("#btn-4").click(function() {
+        $("#btn-4 .inner").css({
+           "background-color": "#DC3522" 
+        });
+        $("#btn-1 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-2 .inner").css({
+           "background-color": "white" 
+        });
+        $("#btn-3 .inner").css({
+           "background-color": "white" 
+        });
+        
+        
+        $("#img-4").css({
+           "opacity": "1",
+           "z-index": "20"
+        });
+        $("#img-1").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-2").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+        $("#img-3").css({
+           "opacity": "0",
+           "z-index": "1"
+        });
+    });
+    
+});
